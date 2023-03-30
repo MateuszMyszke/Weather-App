f08f68578c194628c7341b5dfd4e25c92396345e
Mateusz Myszke <myszke93@gmail.com>
2023-03-30 12:21:04 +0200

    Add setPending

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,29 +1,38 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 
 
 const WeatherBox = props => {
-
+  const [weather, setWeather] = useState('');
+  const [pending, setPending] = useState(false)
   const handleCityChange = useCallback(city => {
     console.log(city);
+    setPending(true);
 
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2285da7b9cbf8d6de02b2aebdba1affc&units=metric`)
-   .then(res => res.json())
-   .then(data => {
-     console.log(data);
-   })
-   .catch(err => {alert(`Error: ${err}`);});
-  }
+    .then(res => res.json())
+    .then(data => {
+    const weatherData = {
+      city: data.name,
+      temp: data.main.temp,
+      icon: data.weather[0].icon,
+      description: data.weather[0].main
+      };
+      setWeather(weatherData);
+      setPending(false);
+    })
+    .catch(err => {alert(`Error: ${err}`);});
+    }
   ); 
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary />
-      <Loader />
+      {weather && !pending && <WeatherSummary {...weather} /> }
+      {pending && <Loader /> }
     </section>
   )
 };
