56d242eff9d8a372a18f2c6d779ae6b7447a0ead
Mateusz Myszke <myszke93@gmail.com>
2023-03-30 11:15:20 +0200

    Add callBack and add fetch

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,27 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
+
+
 
 const WeatherBox = props => {
+
+  const handleCityChange = useCallback(city => {
+    console.log(city);
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2285da7b9cbf8d6de02b2aebdba1affc&units=metric`)
+   .then(res => res.json())
+   .then(data => {
+     console.log(data);
+   })
+   .catch(err => {alert(`Error: ${err}`);});
+  }
+  ); 
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>
